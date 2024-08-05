ce0726f733d7ed4fcc4a0d6db9c91708e0a179d1
Arthur <109326021+nullpeer@users.noreply.github.com>
2024-08-06 02:11:06 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,10 +5,10 @@ export default class About extends Component {
     return (
       <main>
         <div className="main-container">
-            <h2 className="center">Обо мне</h2>
-            <p>Меня зовут Игорь. Я — React-разработчик из России. Начинающий, но уже разбираюсь во многих "штучках" Реакта.</p>
-            <p>В данный момент я развиваюсь, изучая React всё глубже и глубже, а также изучаю немецкий язык.</p>
-            <p>Эта визитка — мой первый серьёзный проект, в котором я подогнал стили и под мобильные устройства.</p>
+            <h2 className="center">About me</h2>
+            <p>It’s Arthur. He is a React-developer from Russia. Rookie, but already know some kinda stuff of React.</p>
+            <p>Now he is developing in many areas</p>, learning React more deeper and deeper.</p>
+            <p>This business card — Arthur’s first serious project, in which he made the adaptive layout.</p>
         </div>
         
       </main>
